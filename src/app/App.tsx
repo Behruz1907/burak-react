9ff94e0ad7ca4e174ec0ab2a5c4dd930c07a05e6
ryan <behruzanorboev1@gmail.com>
2026-06-25 02:23:11 +0900

    BRR: Initial Setup

---
+++ src/app/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "../css/app.css";
+
+function App() {
+  return (
+    <div>
+      HOME,
+      <div className="test">
+        Css ishlebdi lekin VS CODDA XATOLIK CHIQIB TURIBDI QIZIL BOLIB VA
+        tsconfig.json faylida ham 2 ta hatolik turibdi kod ishlagani uchun
+        teginmadim
+      </div>
+    </div>
+  );
+}
+
+export default App;
