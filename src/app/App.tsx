9e82d92d4d0e03968caa361d05235387b5217f63
ryan <behruzanorboev1@gmail.com>
2026-06-30 05:24:03 +0900

    feat: integration  footer and  basket source

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,17 +1,20 @@
 import React from "react";
 import { Box, Button, Container, Stack, Typography } from "@mui/material";
 import { Link, Route, Switch, useLocation } from "react-router-dom";
-import { HomePage } from "./screens/homePage";
-import { ProductsPage } from "./screens/productsPage";
-import { OrdersPage } from "./screens/ordersPage";
-import { UserPage } from "./screens/userPage";
-import { HomeNavbar } from "./components/headers/HomeNavbar";
-import { OtherNavbar } from "./components/headers/OtherNavbar";
-import { Footer } from "./components/footer";
+import HomePage from "./screens/homePage";
+import ProductsPage from "./screens/productsPage";
+import OrdersPage from "./screens/ordersPage";
+import UserPage from "./screens/userPage";
+import HomeNavbar from "./components/headers/HomeNavbar";
+import OtherNavbar from "./components/headers/OtherNavbar";
+import Footer from "./components/footer";
 // @ts-ignore: Allow side-effect CSS import without type declarations
 import "../css/app.css";
 // @ts-ignore: Allow side-effect CSS import without type declarations
 import "../css/navbar.css";
+// @ts-ignore: Allow side-effect CSS import without type declaration
+import "../css/footer.css";
+
 import { HelpPage } from "./screens/helpPage";
 
 function App() {
