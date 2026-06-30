750dc87db53aa9a466c93edf1f11b5f3322d1123
ryan <behruzanorboev1@gmail.com>
2026-06-30 16:25:13 +0900

    fix: modify statistics sectional component

--- src/app/screens/homePage/index.tsx
+++ src/app/screens/homePage/index.tsx
@@ -5,6 +5,8 @@ import NewDishes from "./NewDishes";
 import Advertisement from "./Advertisement";
 import ActiveUsers from "./ActiveUsers";
 import Events from "./Events";
+// @ts-ignore: Allow side-effect CSS import without type declaration
+import "../../../css/home.css";
 
 export default function HomePage() {
   return (
