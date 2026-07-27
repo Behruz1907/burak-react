7ce8a85a07c9b641ca7e0eef34619d57802c0e05
ryan <behruzanorboev1@gmail.com>
2026-07-28 01:19:25 +0900

    feat: create signup process

--- src/app/components/headers/HomeNavbar.tsx
+++ src/app/components/headers/HomeNavbar.tsx
@@ -10,29 +10,23 @@ interface HomeNavbarProps {
   onRemove: (item: CartItem) => void;
   onDelete: (item: CartItem) => void;
   onDeleteAll: () => void;
+  setSignupOpen: (isOpen: boolean) => void;
+  setLoginOpen: (isOpen: boolean) => void;
 }
 
 export default function HomeNavbar(props: HomeNavbarProps) {
-  const { cartItems, onAdd, onRemove, onDelete, onDeleteAll } = props;
+  const {
+    cartItems,
+    onAdd,
+    onRemove,
+    onDelete,
+    onDeleteAll,
+    setSignupOpen,
+    setLoginOpen,
+  } = props;
   const authMember = null;
-  const [count, setCount] = useState<number>(0);
-  const [value, setvalue] = useState<boolean>(true);
-
-  useEffect(() => {
-    console.log("componentDidMount");
-    setCount(count + 1);
-    return () => {
-      console.log("componentWillUnmount");
-    };
-    // BACKEDNDAN MALUMOTNI OLIB KELGANDA ISHLATAMIZ
-  }, [value]); // doim 1 marta ishga tushadi lekin array dependency ga malum
-  // bir valueni qiymatini kiritsak valueani qiymatini ozgarganda useEffect yana ishga tushadi
 
   /**HANDLERS */
-
-  const buttonHandler = () => {
-    setvalue(!value);
-  };
 
   return (
     <div className="home-navbar">
@@ -83,7 +77,11 @@ export default function HomeNavbar(props: HomeNavbarProps) {
 
             {!authMember ? (
               <Box>
-                <Button variant="contained" className="login-button">
+                <Button
+                  variant="contained"
+                  className="login-button"
+                  onClick={() => setLoginOpen(true)}
+                >
                   Login
                 </Button>
               </Box>
@@ -102,13 +100,13 @@ export default function HomeNavbar(props: HomeNavbarProps) {
               World's Most Delicious Cousine
             </Box>
             <Box className={"wel-txt"}>The Choice, not just a choice</Box>
-            <Box className={"service-txt"}>{count} hours service</Box>
+            <Box className={"service-txt"}>24 hours service</Box>
             <Box className={"signup"}>
               {!authMember ? (
                 <Button
                   variant={"contained"}
                   className={"signup-button"}
-                  onClick={buttonHandler}
+                  onClick={() => setSignupOpen(true)}
                 >
                   SIGN UP
                 </Button>
--- src/app/services/MemberService.ts
+++ src/app/services/MemberService.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { serverApi } from "../../lib/config";
-import { Member } from "../../lib/types/member";
+import { Member, MemberInput } from "../../lib/types/member";
 
 class MemberService {
   private readonly path: string;
@@ -30,6 +30,25 @@ class MemberService {
       return restaurant;
     } catch (err) {
       console.log("Error,  getRestaurant:", err);
+      throw err;
+    }
+  }
+
+  // localstorage ga ham member datani yozvoldik ==? brauzerni ixtiyoriy
+  // burchagida authenticatet bolgan memberni malumotlari kerak boladi image, etc
+  public async signup(input: MemberInput): Promise<Member> {
+    try {
+      const url = this.path + "/member/signup";
+      const result = await axios.post(url, input, { withCredentials: true });
+      console.log("signup:", result);
+      const member: Member = result.data.member;
+      console.log("member:", member);
+
+      localStorage.setItem("memberData", JSON.stringify(member));
+
+      return member;
+    } catch (err) {
+      console.log("Error,  signup:", err);
       throw err;
     }
   }
